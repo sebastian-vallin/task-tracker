f989a0cf42e565687ab5ae1cfea977db00174246
Sebastian Vallin <sebastian.vallin@lexicon.se>
2021-12-08 15:47:00 +0100

    Add null check to tasks component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,10 @@
 import PropTypes from "prop-types";
 
 function Tasks({ tasks }) {
+  if (!tasks || tasks.length === 0) {
+    return <p>No tasks to display</p>;
+  }
+
   return tasks.map((task) => <p key={task.id}>{task.text}</p>);
 }
 
