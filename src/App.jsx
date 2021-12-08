ffd095204d08fbfe84f91d19fb8084bb918584b6
Sebastian Vallin <sebastian.vallin@lexicon.se>
2021-12-08 15:05:43 +0100

    Add Header and Button components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import Header from "./components/Header";
+
+const initialTasks = [
+  {
+    id: 1,
+    text: "Task 1",
+    day: "Dec 10th at 2:00pm",
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: "Task 2",
+    day: "Dec 12th at 5:00pm",
+    reminder: false,
+  },
+  {
+    id: 3,
+    text: "Task 3",
+    day: "Dec 10th at 9:00am",
+    reminder: true,
+  },
+];
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="container">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+function Header({ title }) {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green">Add</Button>
+    </header>
+  );
+}
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string,
+};
+
+export default Header;
