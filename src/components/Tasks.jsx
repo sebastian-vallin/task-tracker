4b4966ea7957bf801a8c244c2727c8a6285fcab6
Sebastian Vallin <sebastian.vallin@lexicon.se>
2021-12-09 14:45:17 +0100

    Add task component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,11 +1,12 @@
 import PropTypes from "prop-types";
+import Task from "./Task";
 
 function Tasks({ tasks }) {
   if (!tasks || tasks.length === 0) {
     return <p>No tasks to display</p>;
   }
 
-  return tasks.map((task) => <p key={task.id}>{task.text}</p>);
+  return tasks.map((task) => <Task key={task.id} task={task} />);
 }
 
 Tasks.defaultProps = {
